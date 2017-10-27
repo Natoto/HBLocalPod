3dd96e39a0e7856addb6fb8923f6e5ef72684227
huangbo <huangbo@yy.com>
2017-10-27 11:05:24 +0800

    UPDATE

---
+++ HBStickerView/PPStickImageView.h
@@ -0,0 +1,13 @@
+//
+//  PPDrawImageView.h
+//  POPA
+//
+//  Created by boob on 2017/10/3.
+//  Copyright © 2017年 YY.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPStickImageView : UIImageView
+
+@end
---
+++ HBStickerView/PPStickImageView.m
@@ -0,0 +1,22 @@
+
+//
+//  PPDrawImageView.m
+//  POPA
+//
+//  Created by boob on 2017/10/3.
+//  Copyright © 2017年 YY.COM. All rights reserved.
+//
+
+#import "PPStickImageView.h"
+
+@implementation PPStickImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
